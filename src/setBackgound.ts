9b748bc0953f63cff28e9b27317eeb827115841e
Ho Tran <tran.ngoc.ho89@gmail.com>
2017-02-26 09:24:38 +0700

    Update directive

--- src/setBackgound.ts
+++ src/setBackgound.ts
@@ -5,8 +5,10 @@ import { Directive, Input, ElementRef, AfterViewInit } from "@angular/core";
 })
 
 export class SetBackground implements AfterViewInit {
-    @Input() color: string;
-    @Input() defaultColor: string;
+    // @Input() color: string;
+    // @Input() defaultColor: string;
+    @Input("setBackground") color: string;
+    @Input("defaultColor") defaultColor: string;
 
     private ui: ElementRef = null;
     constructor(ui: ElementRef) {
